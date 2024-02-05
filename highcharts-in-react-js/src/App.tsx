72a20de8d87615e7b4f345655d703ccb13351dfc
Cristiano da Silva Monteiro <cristianodasilvamonteiro@gmail.com>
2024-02-05 07:22:56 -0300

    add Venn Diagram and Treegraph Chart

---
+++ highcharts-in-react-js/src/App.tsx
@@ -0,0 +1,14 @@
+import './Global.css';
+
+import VennDiagram from './Charts/VennDiagram/VennDiagram';
+import TreegraphChart from './Charts/TreegraphChart/TreegraphChart';
+
+export default function App() {
+  return (
+    <>
+      <h1>teste</h1>
+      <VennDiagram/>
+      <TreegraphChart/>
+    </>
+  )
+};
