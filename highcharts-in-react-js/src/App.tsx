92e365ef3796e2b06d636dc016d039373cab5d2e
Cristiano da Silva Monteiro <cristianodasilvamonteiro@gmail.com>
2024-02-19 12:32:24 -0300

    map chart - feb 19, 2024

--- highcharts-in-react-js/src/App.tsx
+++ highcharts-in-react-js/src/App.tsx
@@ -2,6 +2,11 @@ import './Global.css';
 
 import VennDiagram from './Charts/VennDiagram/VennDiagram';
 import TreegraphChart from './Charts/TreegraphChart/TreegraphChart';
+import DependencyWheel from './Charts/DependencyWheel/DependencyWheel';
+import TreegraphBoxLayout from './Charts/TreegraphBoxLayout/TreegraphBoxLayout';
+
+import TiledWebMap from './Charts/Maps/TiledWebMap/TiledWebMap';
+import MapWithMarkerClusters from './Charts/Maps/MapWithMarkerClusters/MapWithMarkerClusters';
 
 export default function App() {
   return (
@@ -9,6 +14,12 @@ export default function App() {
       <h1>teste</h1>
       <VennDiagram/>
       <TreegraphChart/>
+      <DependencyWheel/>
+      <TreegraphBoxLayout/>
+      {/* <TiledWebMap/> */}
+
+      <MapWithMarkerClusters/>
+
     </>
   )
 };
